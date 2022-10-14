09904d33c10999363a90f48ec1745e08e2af7e04
DamienFoulon <44929858+DamienFoulon@users.noreply.github.com>
2022-10-14 11:07:00 +0200

    setup camera

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -1,8 +1,12 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-  console.log(this.responseText);
- }
+    if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText);
+    } else {
+    console.log(this.status);
+    console.log(this.responseText);
+    console.log(this.readyState);
+    }
 };
 xhttp.open("GET", "http://192.168.2.197:8080/about", true);
 xhttp.send();
